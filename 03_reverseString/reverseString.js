326e10d4a75c4c59024c922d100220dbce140fff
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-02-17 18:40:10 -0500

    Add initial solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // initial solution
+    // let chars = str.split('');
+    // let result = "";
+    // for (i = chars.length - 1; i >= 0; i--){
+    //     result += chars[i];
+    // }
+    // return result;
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
