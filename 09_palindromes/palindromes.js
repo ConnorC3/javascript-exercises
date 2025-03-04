5a8fc1928cdc03916b4a7c6ca7660ee5fbd32b44
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-03-03 20:41:05 -0500

    Initial solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    // change to lowercase
+    str = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumeric.includes(char))
+        .join('');
 
+    // check if palindrome
+    let l = 0, r = str.length - 1;
+    while (l < r){
+        if (str[l] !== str[r]){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
