df4a197e734f4e09432a5046eda826dfef742042
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-03-03 20:53:56 -0500

    Initial solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n; // convert to int
+    if (n < 0) return "OOPS"; // check for negatives
+
+    if (n === 1 || n === 2) return 1; 
+
+
+    let fib1 = 1, fib2 = 1;
+    let sum = 0;
+
+    for (let i = 2; i < n; i++){
+        sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = sum;
+    }
+
+    return sum;
 
 };
 
